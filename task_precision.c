8f25815d72878f624f56db5920aadc11cf99e00b
Afanyu Lionel <134651486+afanyulionel@users.noreply.github.com>
2023-07-25 22:55:55 +0100

    Update task_precision.c - to > normal

--- task_precision.c
+++ task_precision.c
@@ -1,50 +1,41 @@
 #include "main.h"
 
 /**
- * handle_precision - Checks precision validity
- * @conv: Conversion specifier  
- * Return: 1 if valid, 0 if invalid
-*/
-int handle_precision(char conv)
-{
-        int i;
-        char *valid_conversions = "scdiouxXeEfFgGaA%";
-
-        for (i = 0; valid_conversions[i]; i++)
-        {
-                if (conv == valid_conversions[i])
-                        return (1);
-        }
-
-        return (0);
-}
-
-/**
- * get_precision - Parses precision from format string
- * @format: Format string
- * @i: Pointer to current index
- * @list: Argument list
- * Return: Precision value or -1 if invalid
-*/
+ * get_precision - Calculates the precision for printing
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
+ *
+ * Return: Precision.
+ */
 int get_precision(const char *format, int *i, va_list list)
 {
-        int curr_i, precision = -1;
+	int curr_i = *i + 1;
+	int precision = -1;
 
-        if (format[*i + 1] != '.')
-                return (precision);
+	if (format[curr_i] != '.')
+		return (precision);
 
-        precision = 0;
+	precision = 0;
 
-        for (curr_i = *i + 2; format[curr_i]; curr_i++)
-        {
-                if (is_digit(format[curr_i]))
-                        precision = (precision * 10) + (format[curr_i] - '0'); 
-                else if (format[curr_i] == '*')
-                        precision = va_arg(list, int);
-                else if (!handle_precision(format[curr_i]))
-                        return (-1);
-        }
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	{
+		if (is_digit(format[curr_i]))
+		{
+			precision *= 10;
+			precision += format[curr_i] - '0';
+		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			precision = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
 
-       *i = curr_i - 1;
-       return (precision);
+	*i = curr_i - 1;
+
+	return (precision);
 }
