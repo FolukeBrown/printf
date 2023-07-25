0ddd6cadb1d04208566468019708ffe73355dae8
Afanyu Lionel <134651486+afanyulionel@users.noreply.github.com>
2023-07-25 20:54:04 +0100

    update: new, check format for '.'

--- task_precision.c
+++ task_precision.c
@@ -1,9 +1,17 @@
+/**
+ * get_precision - Gets the precision for printing from format string
+ * @format: Formatted string containing format specifiers
+ * @i: Pointer to current index in format string 
+ * @list: Argument list
+ *
+ * Return: Precision value, or -1 on error
+ */
 int get_precision(const char *format, int *i, va_list list)
 {
 	int precision = -1;
 	int curr_i = 0;
 
-	// Parse digits before '.' as precision
+	/* Parse digits before '.' as precision */
 	curr_i = *i + 1;
 	while (is_digit(format[curr_i]))
 	{
@@ -11,20 +19,20 @@ int get_precision(const char *format, int *i, va_list list)
 		curr_i++;
 	}
 
-	// Check for '.' 
+	/* Check for '.' */
 	if (format[curr_i] == '.')
 	{
 		curr_i++;
-		
-		// Parse digits after '.' as precision
-		while (is_digit(format[curr_i]))
+
+		/* Parse digits after '.' as precision */
+		while (is_digit(format[curr_i])) 
 		{
 			precision = precision * 10 + (format[curr_i] - '0');
 			curr_i++;
 		}
 	}
 
-	// Update format index
+	/* Update format index */
 	*i = curr_i - 1;
 
 	return (precision);
