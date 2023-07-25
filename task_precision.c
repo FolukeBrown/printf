0d3845ecc07663c0746f6aa2b0f8372b6eb51389
Afanyu Lionel <134651486+afanyulionel@users.noreply.github.com>
2023-07-25 22:28:30 +0100

    Update task_precision.c - rewrite handle_precision

--- task_precision.c
+++ task_precision.c
@@ -1,5 +1,27 @@
 #include "main.h"
 
+/** 
+ * handle_precision - Checks if precision is valid for a conversion
+ * @conv: The conversion specifier character
+ *
+ * Return: 1 if precision is valid, 0 if invalid
+*/
+int handle_precision(char conv)
+{
+        int i;
+        char *valid_conversions = "scdiouxXeEfFgGaA%";
+
+        for (i = 0; valid_conversions[i]; i++) 
+        {
+                if (conv == valid_conversions[i])
+                {
+                        return (1);
+                }
+        }
+
+        return (0);
+}
+
 /**
  * get_precision - Calculates the precision for printing
  * @format: Formatted string in which to print the arguments
@@ -10,32 +32,36 @@
  */
 int get_precision(const char *format, int *i, va_list list)
 {
-	int curr_i = *i + 1;
-	int precision = -1;
+        int curr_i = *i + 1;
+        int precision = -1;
 
-	if (format[curr_i] != '.')
-		return (precision);
+        if (format[curr_i] != '.')
+        {
+                return (precision);
+        }
 
-	precision = 0;
+        precision = 0;
 
-	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
-	{
-		if (is_digit(format[curr_i]))
-		{
-			precision *= 10;
-			precision += format[curr_i] - '0';
-		}
-		else if (format[curr_i] == '*')
-		{
-			curr_i++;
-			precision = va_arg(list, int);
-			break;
-		}
-		else
-			break;
-	}
+        for (curr_i += 1; format[curr_i]; curr_i++)
+        {
+                if (is_digit(format[curr_i]))
+                {
+                        precision = precision * 10 + (format[curr_i] - '0');
+                }
+                else if (format[curr_i] == '*') 
+                {
+                        curr_i++;
+                        precision = va_arg(list, int);
+                        break;
+                }
+                else if (!handle_precision(format[curr_i]))
+                {
+                        precision = -1;
+                        break;
+                }
+        }
 
-	*i = curr_i - 1;
-
-	return (precision);
+        *i = curr_i - 1;
+	
+        return (precision);  
 }
