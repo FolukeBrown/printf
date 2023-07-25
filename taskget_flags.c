1d1d11d6ce23d0fd87daf50754280c6b46f01f6e
Afanyu Lionel <134651486+afanyulionel@users.noreply.github.com>
2023-07-25 19:30:22 +0100

    Update taskget_flags.c
    modify self-naming of local variables

--- taskget_flags.c
+++ taskget_flags.c
@@ -8,15 +8,15 @@
  */
 int get_flags(const char *format, int *i)
 {
-	int j, curr_i;
+	int j, self_i;
 	int flags = 0;
 	const char FLAGS_CHAR[] = {'-', '+', '0', '#', ' ', '\0'};
 	const int FLAGS_ARRAY[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
-	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	for (self_i = *i + 1; format[self_i] != '\0'; self_i++)
 	{
 		for (j = 0; FLAGS_CHAR[j] != '\0'; j++)
-			if (format[curr_i] == FLAGS_CHAR[j])
+			if (format[self_i] == FLAGS_CHAR[j])
 			{
 				flags |= FLAGS_ARRAY[j];
 				break;
@@ -26,7 +26,7 @@ int get_flags(const char *format, int *i)
 			break;
 	}
 
-	*i = curr_i - 1;
+	*i = self_i - 1;
 
 	return (flags);
 }
