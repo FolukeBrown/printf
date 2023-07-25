48787bd04ab17556b636c84877e01fa137e98796
Afanyu Lionel <134651486+afanyulionel@users.noreply.github.com>
2023-07-25 19:27:30 +0100

    Update taskget_flags.c:
    Modify flag char and flags array

--- taskget_flags.c
+++ taskget_flags.c
@@ -12,19 +12,19 @@ int get_flags(const char *format, int *i)
 	/* 1 2 4 8  16 */
 	int j, curr_i;
 	int flags = 0;
-	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
-	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+	const char FLAGS_CHAR[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLAGS_ARRAY[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
 	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
-		for (j = 0; FLAGS_CH[j] != '\0'; j++)
-			if (format[curr_i] == FLAGS_CH[j])
+		for (j = 0; FLAGS_CHAR[j] != '\0'; j++)
+			if (format[curr_i] == FLAGS_CHAR[j])
 			{
-				flags |= FLAGS_ARR[j];
+				flags |= FLAGS_ARRAY[j];
 				break;
 			}
 
-		if (FLAGS_CH[j] == 0)
+		if (FLAGS_CHAR[j] == 0)
 			break;
 	}
 
